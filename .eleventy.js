dc78ee81e0c5a5ca9b845411d554c893dc49a782
cmlenius <cameron.lenius@gmail.com>
2024-03-01 19:29:40 +0100

    init

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.setBrowserSyncConfig({
+    files: "./public/static/**/*.css",
+  });
+  return {
+    dir: {
+      input: "src",
+      output: "docs",
+    },
+  };
+};
